df2b9799a6545435fcdeaa9567667c4730533bc4
XF-202204051732\Administrator <404669366@qq.com>
2022-10-28 15:48:44 +0800

    gorm日志

--- mysql.go
+++ mysql.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"io"
+	"log"
 	"time"
 )
 
@@ -19,11 +22,18 @@ type MysqlConfig struct {
 	Timezone     string
 	MaxIdleConns int
 	MaxOpenConns int
+	LogWriters   []io.Writer
+	LogConfigs   logger.Config //SlowThreshold 慢SQL阈值,LogLevel 日志级别,IgnoreRecordNotFoundError忽略ErrRecordNotFound(记录未找到)错误,Colorful 禁用彩色打印
 }
 
 func InitMysql(config *MysqlConfig) {
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%v)/%s?charset=%s&parseTime=true&loc=%s", config.Username, config.Password, config.Host, config.Port, config.Database, config.Charset, config.Timezone)
-	instance, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	instance, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: logger.New(
+			log.New(io.MultiWriter(config.LogWriters...), "\r\n", log.LstdFlags),
+			config.LogConfigs,
+		),
+	})
 	if err != nil {
 		panic("Connect db error:\n" + err.Error())
 	}
