e25f7282dbd5ab0bfbbd045a6196585e06dbef92
XF-202204051732\Administrator <404669366@qq.com>
2022-11-04 15:26:00 +0800

    gorm 类型补充

--- gorm.go
+++ gorm.go
@@ -1,6 +1,7 @@
 package tools
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -57,4 +58,70 @@ func InitGorm(dialector gorm.Dialector, MaxIdleConns, MaxOpenConns int, ConnMaxL
 	pool.SetMaxOpenConns(MaxOpenConns)
 	pool.SetConnMaxLifetime(ConnMaxLifetime)
 	return instance
+}
+
+func DateNow() *Date {
+	now := Date(time.Now())
+	return &now
+}
+
+type Date time.Time
+
+func (t *Date) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(*t)
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format(DateFormat))), nil
+}
+
+func (t *Date) Scan(v interface{}) error {
+	if value, ok := v.(time.Time); ok {
+		*t = Date(value)
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+func (t Date) Value() (driver.Value, error) {
+	tm := time.Time(t)
+	var zero time.Time
+	if tm.UnixNano() == zero.UnixNano() {
+		return nil, nil
+	}
+	return tm, nil
+}
+
+func (t Date) ToTime() time.Time {
+	return time.Time(t)
+}
+
+func DateTimeNow() *DateTime {
+	now := DateTime(time.Now())
+	return &now
+}
+
+type DateTime time.Time
+
+func (t *DateTime) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(*t)
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format(DatetimeFormat))), nil
+}
+
+func (t *DateTime) Scan(v interface{}) error {
+	if value, ok := v.(time.Time); ok {
+		*t = DateTime(value)
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+func (t DateTime) Value() (driver.Value, error) {
+	tm := time.Time(t)
+	var zero time.Time
+	if tm.UnixNano() == zero.UnixNano() {
+		return nil, nil
+	}
+	return tm, nil
+}
+
+func (t DateTime) ToTime() time.Time {
+	return time.Time(t)
 }
