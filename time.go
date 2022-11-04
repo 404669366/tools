7299158041171a9c7683ade57b2ba9b453e885d3
XF-202204051732\Administrator <404669366@qq.com>
2022-11-04 11:33:34 +0800

    时间

--- time.go
+++ time.go
@@ -1,6 +1,18 @@
 package tools
 
+import (
+	"fmt"
+	"time"
+)
+
 const (
 	DateFormat     = "2006-01-02"
 	DatetimeFormat = "2006-01-02 15:04:05"
 )
+
+type DateTime time.Time
+
+func (lt *DateTime) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(*lt)
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format(DatetimeFormat))), nil
+}
