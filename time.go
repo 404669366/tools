3e5c705c870e3311d235ad1455976f331d42a3e1
XF-202204051732\Administrator <404669366@qq.com>
2022-11-04 13:29:29 +0800

    时间

--- time.go
+++ time.go
@@ -1,6 +1,7 @@
 package tools
 
 import (
+	"database/sql/driver"
 	"fmt"
 	"time"
 )
@@ -15,21 +16,63 @@ func DateNow() *Date {
 	return &now
 }
 
+type Date time.Time
+
+func (t *Date) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(*t)
+	return []byte(fmt.Sprintf("\"%v\"", tTime.Format(DateFormat))), nil
+}
+
+func (t *Date) Scan(v interface{}) error {
+	if value, ok := v.(time.Time); ok {
+		*t = Date(value)
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+func (t Date) Value() (driver.Value, error) {
+	tm := time.Time(t)
+	var zero time.Time
+	if tm.UnixNano() == zero.UnixNano() {
+		return nil, nil
+	}
+	return tm, nil
+}
+
+func (t Date) ToTime() time.Time {
+	return time.Time(t)
+}
+
 func DateTimeNow() *DateTime {
 	now := DateTime(time.Now())
 	return &now
 }
 
-type Date time.Time
-
-func (lt *Date) MarshalJSON() ([]byte, error) {
-	tTime := time.Time(*lt)
-	return []byte(fmt.Sprintf("\"%v\"", tTime.Format(DateFormat))), nil
-}
-
 type DateTime time.Time
 
-func (lt *DateTime) MarshalJSON() ([]byte, error) {
-	tTime := time.Time(*lt)
+func (t *DateTime) MarshalJSON() ([]byte, error) {
+	tTime := time.Time(*t)
 	return []byte(fmt.Sprintf("\"%v\"", tTime.Format(DatetimeFormat))), nil
+}
+
+func (t *DateTime) Scan(v interface{}) error {
+	if value, ok := v.(time.Time); ok {
+		*t = DateTime(value)
+		return nil
+	}
+	return fmt.Errorf("can not convert %v to timestamp", v)
+}
+
+func (t DateTime) Value() (driver.Value, error) {
+	tm := time.Time(t)
+	var zero time.Time
+	if tm.UnixNano() == zero.UnixNano() {
+		return nil, nil
+	}
+	return tm, nil
+}
+
+func (t DateTime) ToTime() time.Time {
+	return time.Time(t)
 }
